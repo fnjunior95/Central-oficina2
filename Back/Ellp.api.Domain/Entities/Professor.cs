10cfa50d3f6c2250c337f74fa551290809845763
Eduardo Gabriel de Freitas <58999344+Eduardogdfrt@users.noreply.github.com>
2024-11-29 16:12:16 -0300

    dsadsa

--- Back/Ellp.api.Domain/Entities/Professor.cs
+++ Back/Ellp.api.Domain/Entities/Professor.cs
@@ -5,6 +5,14 @@ namespace Ellp.Api.Domain.Entities
 {
     public class Professor
     {
+        public Professor(int professorId, string name, string specialty, string password, string email)
+        {
+            ProfessorId = professorId;
+            Name = name;
+            Specialty = specialty;
+            Password = password;
+            Email = email;
+        }
         public int ProfessorId { get; set; }
         public string Name { get; set; }
         public string Specialty { get; set; }
--- Back/ellp.api.application/UseCases/AddParticipantUsecases/AddNewProfessorUseCases/AddNewPorfessorInput.cs
+++ Back/ellp.api.application/UseCases/AddParticipantUsecases/AddNewProfessorUseCases/AddNewPorfessorInput.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ellp.Api.Application.UseCases.AddParticipantUsecases.AddNewProfessorUseCases
 {
-    internal class AddNewPorfessorInput
+    public class AddNewProfessorInput : IRequest<AddNewProfessorMapper>
     {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Specialty { get; set; }
     }
 }
+
